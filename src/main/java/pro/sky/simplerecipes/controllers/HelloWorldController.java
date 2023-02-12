073c3ce34bf774c0ba70d42e70a8883f1b80d820
Denis <9150771050@mail.ru>
2023-02-13 00:04:47 +0300

    12.02.23

--- src/main/java/pro/sky/simplerecipes/controllers/HelloWorldController.java
+++ src/main/java/pro/sky/simplerecipes/controllers/HelloWorldController.java
@@ -1,2 +1,19 @@
-package pro.sky.simplerecipes.controllers;public class HelloWorldController {
+package pro.sky.simplerecipes.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping(path = "*")
+    public String greeting() {
+        return "Приложение запущено!";
+    }
+
+    @GetMapping("/info")
+    public String info() {
+        return "Имя ученика: Евгений Непочатых; Название проекта: Простые рецепты; Дата создания проекта: 12.02.2023; Описание проекта: Приложение для сайта рецептов.";
+    }
 }
